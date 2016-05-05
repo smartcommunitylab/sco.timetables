b81fdd26be9e9d44ad90b32126982537ba0cad19
Роман <kazhamiakin@gmail.com>
2016-05-05 18:47:04 +0200

    Initial import

---
+++ orariapp/www/js/controllers/CommonCtrl.js
@@ -0,0 +1,134 @@
+angular.module('viaggia.controllers.common', [])
+
+.controller('AppCtrl', function ($scope, $state, $rootScope, $location, $timeout, DataManager, $ionicPopup, $ionicModal, $filter, $ionicLoading, Config) {
+    /*menu group*/
+    $scope.shownGroup = false;
+    $scope.toggleGroupRealTime = function () {
+        if ($scope.isGroupRealTimeShown()) {
+            $scope.shownGroup = false;
+        } else {
+            $scope.shownGroup = true;
+        }
+        localStorage.setItem(Config.getAppId() + '_shownGroup', $scope.shownGroup);
+
+    };
+    $scope.isGroupRealTimeShown = function () {
+        return $scope.shownGroup === true;
+    };
+
+    $ionicModal.fromTemplateUrl('templates/credits.html', {
+        id: '3',
+        scope: $scope,
+        backdropClickToClose: false,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.creditsModal = modal;
+    });
+    $scope.closeCredits = function () {
+        $scope.creditsModal.hide();
+    };
+    $scope.openCredits = function () {
+            $scope.creditsModal.show();
+        }
+        /*pop up managers*/
+        //    $scope.newPlan = function () {
+        //        planService.setTripId(null); //reset data for pianification
+        //        $state.go('app.plan');
+        //    };
+    $scope.popupLoadingShow = function () {
+        $ionicLoading.show({
+            template: $filter('translate')("pop_up_loading")
+        });
+    };
+    $scope.popupLoadingHide = function () {
+        $ionicLoading.hide();
+    };
+
+    $scope.showConfirm = function (template, title, functionOnTap) {
+        var confirmPopup = $ionicPopup.confirm({
+            title: title,
+            template: template,
+            buttons: [
+                {
+                    text: $filter('translate')("pop_up_cancel"),
+                    type: 'button-cancel'
+                            },
+                {
+                    text: $filter('translate')("pop_up_ok"),
+                    type: 'button-custom',
+                    onTap: functionOnTap
+                    }
+            ]
+        });
+    }
+
+    $scope.showNoConnection = function () {
+        var alertPopup = $ionicPopup.alert({
+            title: $filter('translate')("pop_up_no_connection_title"),
+            template: $filter('translate')("pop_up__no_connection_template"),
+            buttons: [
+                {
+                    text: $filter('translate')("pop_up_ok"),
+                    type: 'button-custom'
+                            }
+            ]
+        });
+    };
+    $scope.showErrorServer = function () {
+        var alertPopup = $ionicPopup.alert({
+            title: $filter('translate')("pop_up_error_server_title"),
+            template: $filter('translate')("pop_up_error_server_template"),
+            buttons: [
+                {
+                    text: $filter('translate')("pop_up_ok"),
+                    type: 'button-custom'
+                            }
+            ]
+        });
+    };
+
+    Config.init().then(function () {
+        $scope.version = Config.getVersion();
+        $scope.shownGroup = JSON.parse(localStorage.getItem(Config.getAppId() + '_shownGroup')) || false;
+        $scope.contactLink = Config.getContactLink();
+    });
+
+    $scope.selectInfomenu = function (m) {
+        //      m.data.label = m.label;
+        //      Config.setInfoMenuParams(m.data);
+        //      $state.go(m.state);
+    };
+})
+
+.factory('Toast', function ($rootScope, $timeout, $ionicPopup, $cordovaToast) {
+    return {
+        show: function (message, duration, position) {
+            message = message || "There was a problem...";
+            duration = duration || 'short';
+            position = position || 'top';
+
+            if (!!window.cordova) {
+                // Use the Cordova Toast plugin
+                $cordovaToast.show(message, duration, position);
+            } else {
+                if (duration == 'short') {
+                    duration = 2000;
+                } else {
+                    duration = 5000;
+                }
+
+                var myPopup = $ionicPopup.show({
+                    template: "<div class='toast'>" + message + "</div>",
+                    scope: $rootScope,
+                    buttons: []
+                });
+
+                $timeout(function () {
+                    myPopup.close();
+                }, duration);
+            }
+        }
+    };
+})
+
+.controller('TutorialCtrl', function ($scope, $ionicLoading) {});
