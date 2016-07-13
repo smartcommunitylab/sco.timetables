a53176a5c920d7d9f597b1a0ad5b2ee58ed99795
Max <max.bolo.mb@gmail.com>
2016-07-13 11:10:27 +0200

    fix

--- orariapp/www/js/controllers/TTCtrl.js
+++ orariapp/www/js/controllers/TTCtrl.js
@@ -132,17 +132,15 @@ angular.module('viaggia.controllers.timetable', ['ionic'])
         $state.go('app.ttmap');
     };
     
-  // SCRIPT ACCESSIBILITA' PER TAB
+  // SCRIPT ACCESSIBILITA'
     $timeout(function(){
-        console.log("sassobuffo loaded");
-        document.getElementsByClassName("tab-item-active")[0].setAttribute("role", "tab");
-        document.getElementsByClassName("tab-item-active")[0].setAttribute("aria-label", "selezionato, i miei viaggi");
-        document.getElementsByClassName("tab-title")[0].setAttribute("aria-label", "true");
+        document.getElementsByClassName("back-text").setAttribute("aria-label", "torna indietro");
+        document.getElementsByClassName("back-text").setAttribute("role", "button");
         
-        console.log($(".tab-item-active").toArray());
-      console.log('loaded');
-      var myEl = angular.element( document.querySelector( '.tab-item' )[0] );
-      myEl.attr('aria-label',"trasporti urbani");
+//      var myEl = angular.element( document.querySelector( '.tab-item' )[0] );
+//      myEl.attr('aria-label',"trasporti urbani");
+//      
+      
       
 //        document.getElementsByClassName("tab-item-active")[0].setAttribute("role", "tab");
 //        document.getElementsByClassName("tab-item-active")[0].setAttribute("aria-label", "trasporti urbani");
