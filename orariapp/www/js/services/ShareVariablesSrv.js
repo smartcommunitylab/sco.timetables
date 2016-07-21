598242dbb857c0b826a9b226eb74eb8b3def5a8e
Michele Schelfi <michele.schelfi@marconirovereto.it>
2016-07-21 15:17:59 +0200

    Re-Add controller in table1.html

---
+++ orariapp/www/js/services/ShareVariablesSrv.js
@@ -0,0 +1,23 @@
+﻿angular.module('viaggia.services.shareVariablesSrv',[])
+
+.service("ShareVariablesSrv", function () {
+    var show;
+    var stops = [];
+    return {
+        setShow: function (s) {
+            show = s;
+        },
+        getShow: function () {
+            return show;
+        },
+        setStops: function (st) {
+            stops = st;
+        },
+        getStops:function(){
+            return stops;
+        },
+        getStop: function (i) {
+            return stops[i];
+        }
+    }
+})
