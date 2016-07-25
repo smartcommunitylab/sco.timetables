84d357df773496ca8a96900385e37351b530a4f1
Michele Schelfi <michele.schelfi@marconirovereto.it>
2016-07-25 13:39:31 +0200

    Beautify

--- orariapp/www/js/controllers/BookmarksCtrl.js
+++ orariapp/www/js/controllers/BookmarksCtrl.js
@@ -1,159 +1,159 @@
 angular.module('viaggia.controllers.bookmarks', [])
 
 	.controller('BookmarksCtrl', function ($scope, $rootScope, $location, $filter, $ionicHistory, $timeout, $ionicModal, $ionicListDelegate, Config, bookmarkService) {
-	    $scope.languageTutorial = "en";
+		$scope.languageTutorial = "en";
         $scope.title = [];
 
-	    $scope.tab = 0;
-	    $scope.tabs = ["FERMATE", "LINEE"];
+		$scope.tab = 0;
+		$scope.tabs = ["FERMATE", "LINEE"];
 
-	    $scope.select = function ($index) {
-	        $scope.tab = $index;
-	        console.log("Tab Index Changed in", $scope.tab);
-	    };
+		$scope.select = function ($index) {
+			$scope.tab = $index;
+			console.log("Tab Index Changed in", $scope.tab);
+		};
 
-	    $scope.$on('$ionicView.beforeEnter', function () {
-	        ('alert 2');
-	    });
-    
-	    $scope.init = function () {
-	        Config.init().then(function () {
-	            bookmarkService.getBookmarks().then(function (list) {
-	                $scope.bookmarks = list;
+		$scope.$on('$ionicView.beforeEnter', function () {
+			('alert 2');
+		});
+
+		$scope.init = function () {
+			Config.init().then(function () {
+				bookmarkService.getBookmarks().then(function (list) {
+					$scope.bookmarks = list;
                     console.log($scope.bookmarks);
                     $scope.getBookmarkTitle(list);
-	            });
-	        });
-	        initTutorial();
-	        setLanguageTutorial();
-	        $scope.showTutorial();
-	    };
-    
-        $scope.getBookmarkTitle = function(list) {
-            for(var key in list) {
+				});
+			});
+			initTutorial();
+			setLanguageTutorial();
+			$scope.showTutorial();
+		};
+
+        $scope.getBookmarkTitle = function (list) {
+            for (var key in list) {
                 var stop = list[key];
-				if(stop.type.indexOf("TRAIN")) {
+				if (stop.type.indexOf("TRAIN")) {
 					var split = stop.data.split(":");
-                	$scope.title.push(split[0].trim() + " - " + Config.getNewDestination(split[1]));
+					$scope.title.push(split[0].trim() + " - " + Config.getNewDestination(split[1]));
 				} else {
 					$scope.title.push(stop.label);
-				}    
-            } 
+				}
+            }
             console.log($scope.title);
         };
-    /*
-        $scope.removeBookmarkTitle = function(idx) {
-            $scope.title.splice(idx, 1);
-        };
-    */
-	    $scope.$on('ngLastRepeat.bookmarks', function (e) {
-	        $timeout(function () {
-	            ionicMaterialMotion.ripple();
-	            ionicMaterialInk.displayEffect()
-	        }); // No timeout delay necessary.
-	    });
-	    $scope.delete = function (idx, $event) {
-	        $scope.deleting = true;
-	        Config.loading();
-	        $event.preventDefault();
-	        bookmarkService.removeBookmark(idx).then(function (list) {
-	            $scope.bookmarks = list;
+		/*
+			$scope.removeBookmarkTitle = function(idx) {
+				$scope.title.splice(idx, 1);
+			};
+		*/
+		$scope.$on('ngLastRepeat.bookmarks', function (e) {
+			$timeout(function () {
+				ionicMaterialMotion.ripple();
+				ionicMaterialInk.displayEffect()
+			}); // No timeout delay necessary.
+		});
+		$scope.delete = function (idx, $event) {
+			$scope.deleting = true;
+			Config.loading();
+			$event.preventDefault();
+			bookmarkService.removeBookmark(idx).then(function (list) {
+				$scope.bookmarks = list;
                 //$scope.removeBookmarkTitle(idx);
-	            $ionicListDelegate.closeOptionButtons();
-	            Config.loaded();
-	        });
-	    };
-	    $scope.reorder = function (from, to) {
-	        Config.loading();
-	        bookmarkService.reorderBookmark(from, to).then(function (list) {
-	            $scope.bookmarks = list;
-	            $ionicHistory.clearCache();
-	            Config.loaded();
-	        });
-	    };
-	    $scope.toggleReorder = function () {
-	        $scope.showReorder = !$scope.showReorder;
-	    }
-	    $scope.go = function (state) {
-	        if ($scope.deleting) {
-	            $scope.deleting = false;
-	        }
-	        else {
-	            $location.path(state);
-	        }
-	    }
-	    $scope.openModal = function () {
-	        $scope.modal.show();
-	    };
-	    $scope.closeModal = function () {
-	        $scope.modal.hide();
-	    };
+				$ionicListDelegate.closeOptionButtons();
+				Config.loaded();
+			});
+		};
+		$scope.reorder = function (from, to) {
+			Config.loading();
+			bookmarkService.reorderBookmark(from, to).then(function (list) {
+				$scope.bookmarks = list;
+				$ionicHistory.clearCache();
+				Config.loaded();
+			});
+		};
+		$scope.toggleReorder = function () {
+			$scope.showReorder = !$scope.showReorder;
+		}
+		$scope.go = function (state) {
+			if ($scope.deleting) {
+				$scope.deleting = false;
+			}
+			else {
+				$location.path(state);
+			}
+		}
+		$scope.openModal = function () {
+			$scope.modal.show();
+		};
+		$scope.closeModal = function () {
+			$scope.modal.hide();
+		};
 
-	    function initTutorial() {
-	        $scope.imageSrc = '';
-	        $scope.showReorder = false;
-	        $scope.tutorialIndex = 1;
-	        $scope.stepTutorial = 2;
-	        $scope.endTutorial = false;
-	    }
+		function initTutorial() {
+			$scope.imageSrc = '';
+			$scope.showReorder = false;
+			$scope.tutorialIndex = 1;
+			$scope.stepTutorial = 2;
+			$scope.endTutorial = false;
+		}
 
-	    function setLanguageTutorial() {
-	        if (typeof navigator.globalization !== "undefined") {
-	            navigator.globalization.getPreferredLanguage(function (language) {
-	                $scope.languageTutorial = language.value.split("-")[0];
-	            }, null);
-	        }
-	    }
+		function setLanguageTutorial() {
+			if (typeof navigator.globalization !== "undefined") {
+				navigator.globalization.getPreferredLanguage(function (language) {
+					$scope.languageTutorial = language.value.split("-")[0];
+				}, null);
+			}
+		}
 
-	    function doTutorial() {
-	        if (!window.localStorage.getItem(Config.getAppId() + '_tutorialDone') || window.localStorage.getItem(Config.getAppId() + '_tutorialDone') == 'false') {
-	            return true;
-	        }
-	        else {
-	            return false;
-	        }
-	    }
-	    $rootScope.forceTutorial = function () {
-	        window.localStorage.setItem(Config.getAppId() + '_tutorialDone', false);
-	        $scope.showTutorial();
-	    }
-	    $scope.showTutorial = function () {
-	        if (doTutorial()) {
-	            if (window.cordova && window.cordova.plugins.screenorientation) {
-	                screen.lockOrientation('portrait');
-	            }
-	            $ionicModal.fromTemplateUrl('templates/bookmarkstutorial.html', {
-	                scope: $scope
+		function doTutorial() {
+			if (!window.localStorage.getItem(Config.getAppId() + '_tutorialDone') || window.localStorage.getItem(Config.getAppId() + '_tutorialDone') == 'false') {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		$rootScope.forceTutorial = function () {
+			window.localStorage.setItem(Config.getAppId() + '_tutorialDone', false);
+			$scope.showTutorial();
+		}
+		$scope.showTutorial = function () {
+			if (doTutorial()) {
+				if (window.cordova && window.cordova.plugins.screenorientation) {
+					screen.lockOrientation('portrait');
+				}
+				$ionicModal.fromTemplateUrl('templates/bookmarkstutorial.html', {
+					scope: $scope
                     , animation: 'slide-in-up'
-	            }).then(function (modal) {
-	                $scope.modal = modal;
-	                initTutorial();
-	                $scope.showImage($scope.tutorialIndex);
-	                $scope.openModal();
-	            });
-	        }
-	        //return true;
-	    }
-	    $scope.closeTutorial = function () {
-	        $scope.closeModal();
-	        if (window.cordova && window.cordova.plugins.screenorientation) {
-	            screen.unlockOrientation()
-	        }
-	        window.localStorage.setItem(Config.getAppId() + '_tutorialDone', true);
-	    }
-	    $scope.nextStep = function () {
-	        if ($scope.tutorialIndex < $scope.stepTutorial) {
-	            $scope.tutorialIndex = $scope.tutorialIndex + 1;
-	            if ($scope.tutorialIndex == $scope.stepTutorial) {
-	                $scope.endTutorial = true;
-	            }
-	            $scope.showImage($scope.tutorialIndex);
-	        }
-	        else {
-	            $scope.closeTutorial();
-	        }
-	    }
-	    $scope.showImage = function (index) {
-	        $scope.imageSrc = 'img/bookmarks/step_' + index + '_' + $scope.languageTutorial + '.png';
-	    }
+				}).then(function (modal) {
+					$scope.modal = modal;
+					initTutorial();
+					$scope.showImage($scope.tutorialIndex);
+					$scope.openModal();
+				});
+			}
+			//return true;
+		}
+		$scope.closeTutorial = function () {
+			$scope.closeModal();
+			if (window.cordova && window.cordova.plugins.screenorientation) {
+				screen.unlockOrientation()
+			}
+			window.localStorage.setItem(Config.getAppId() + '_tutorialDone', true);
+		}
+		$scope.nextStep = function () {
+			if ($scope.tutorialIndex < $scope.stepTutorial) {
+				$scope.tutorialIndex = $scope.tutorialIndex + 1;
+				if ($scope.tutorialIndex == $scope.stepTutorial) {
+					$scope.endTutorial = true;
+				}
+				$scope.showImage($scope.tutorialIndex);
+			}
+			else {
+				$scope.closeTutorial();
+			}
+		}
+		$scope.showImage = function (index) {
+			$scope.imageSrc = 'img/bookmarks/step_' + index + '_' + $scope.languageTutorial + '.png';
+		}
 	})
--- orariapp/www/js/controllers/MapController.js
+++ orariapp/www/js/controllers/MapController.js
@@ -1,30 +1,30 @@
 angular.module('viaggia.controllers.map', [])
 
-.controller('MapController', function($scope, $cordovaGeolocation, $ionicLoading, stopNameSrv, GeoLocate) {
+    .controller('MapController', function ($scope, $cordovaGeolocation, $ionicLoading, stopNameSrv, GeoLocate) {
 
-    $scope.initMap = function(){
-        var directionDisplay = new google.maps.DirectionsRenderer;
-        var directionService = new google.maps.DirectionsService;
-        directionDisplay.setPanel(document.getElementById("directionPanel"));
-        calculateTravel(stopNameSrv.getStop(stopNameSrv.getIndex()),directionDisplay, directionService);
-    }
-     function calculateTravel(destination, directionDisplay, directionService){
-             GeoLocate.locate().then(function(pos){
-                var start = new google.maps.LatLng(pos[0],pos[1]);
+        $scope.initMap = function () {
+            var directionDisplay = new google.maps.DirectionsRenderer;
+            var directionService = new google.maps.DirectionsService;
+            directionDisplay.setPanel(document.getElementById("directionPanel"));
+            calculateTravel(stopNameSrv.getStop(stopNameSrv.getIndex()), directionDisplay, directionService);
+        }
+        function calculateTravel(destination, directionDisplay, directionService) {
+            GeoLocate.locate().then(function (pos) {
+                var start = new google.maps.LatLng(pos[0], pos[1]);
                 var end = new google.maps.LatLng(destination.lat, destination.lng);
-               
+
                 var request = {
                     origin: start,
                     destination: end,
                     travelMode: google.maps.TravelMode.WALKING
                 };
 
-                directionService.route(request, function(res,status){
+                directionService.route(request, function (res, status) {
                     console.log(status);
-                    if(status == google.maps.DirectionsStatus.OK){
+                    if (status == google.maps.DirectionsStatus.OK) {
                         directionDisplay.setDirections(res);
                     }
                 })
-        });
-     }
-});
+            });
+        }
+    });
