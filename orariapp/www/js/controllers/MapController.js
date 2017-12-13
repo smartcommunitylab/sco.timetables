10991fdfc08b6a1588f5481e19998a1832731276
nawaz2013 <nawaz1981@gmail.com>
2017-12-13 14:09:25 +0100

    Fixes for direction popup.

--- orariapp/www/js/controllers/MapController.js
+++ orariapp/www/js/controllers/MapController.js
@@ -1,25 +1,35 @@
 angular.module('viaggia.controllers.map', [])
 
-    .controller('MapController', function ($scope, $cordovaGeolocation, $ionicLoading, $interval, stopNameSrv, GeoLocate) {
+    .controller('MapController', function ($q, $filter, $scope, $cordovaGeolocation, $ionicLoading, $ionicPopup, $interval, stopNameSrv, GeoLocate, Config) {
 
         $scope.initMap = function () {
             Directions();
             console.log("Primoh");
             $interval(function () {
-                Directions();
+                // Directions();
                 console.log("Aggiornizio!");
             }, 30000);
         };
 
         function Directions() {
-            var directionDisplay = new google.maps.DirectionsRenderer;
             var directionService = new google.maps.DirectionsService;
-            document.getElementById("directionPanel").innerHTML = "";
-            directionDisplay.setPanel(document.getElementById("directionPanel"));
-            calculateTravel(stopNameSrv.getStop(stopNameSrv.getIndex()), directionDisplay, directionService);
+           
+            calculateTravel(stopNameSrv.getStop(stopNameSrv.getIndex()), directionService).then(
+
+                function (res) {
+                    var directionDisplay = new google.maps.DirectionsRenderer;
+                    document.getElementById("directionPanel").innerHTML = "";
+                    directionDisplay.setPanel(document.getElementById("directionPanel"));
+                    directionDisplay.setDirections(res);
+                },
+                function (error) {
+                    document.getElementById("directionPanel").innerHTML = $filter('translate')("pop_up_error_server_template");
+                });
         }
 
-        function calculateTravel(destination, directionDisplay, directionService) {
+        function calculateTravel(destination, directionService) {
+            var deferred = $q.defer();
+            Config.loading(); 
             GeoLocate.locate().then(function (pos) {
                 var start = new google.maps.LatLng(pos[0], pos[1]);
                 var end = new google.maps.LatLng(destination.lat, destination.lng);
@@ -32,10 +42,14 @@ angular.module('viaggia.controllers.map', [])
 
                 directionService.route(request, function (res, status) {
                     console.log(status);
+                    Config.loaded();
                     if (status == google.maps.DirectionsStatus.OK) {
-                        directionDisplay.setDirections(res);
+                        deferred.resolve(res);
+                    } else {
+                        deferred.reject();
                     }
                 })
             });
+            return deferred.promise;
         }
     });
